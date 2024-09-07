b547b572c40ab339110ba8b68ec570ad17b1ea09
leordzgcia <leongarzagarza@hotmail.com>
2024-09-07 06:32:37 -0600

    Nueva RFQ tabla

--- src/app/rfq/rfq/rfq.component.ts
+++ src/app/rfq/rfq/rfq.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RfqComponent implements OnInit {
 
+  selectedFiles: File[] = [];
+  banderaTabla = false;
+  
+
+
   constructor() { }
 
   ngOnInit(): void {
@@ -17,6 +22,28 @@ export class RfqComponent implements OnInit {
     $.getScript('./assets/plugins/fancy-file-uploader/jquery.fancy-fileupload.js');
     $.getScript('./assets/plugins/Drag-And-Drop/imageuploadify.min.js');
     $.getScript('./assets/js/custom-file-upload.js');
+  }
+
+  onFilesSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFiles = Array.from(input.files);
+      console.log('Archivos seleccionados:', this.selectedFiles);
+    }
+  }
+
+
+  onUpload(): void {
+    if (this.selectedFiles.length > 0 ) {
+      console.log('Subiendo archivo:', this.selectedFiles);
+      for (let index = 0; index < this.selectedFiles.length; index++) {
+        console.log('Subiendo archivo:', this.selectedFiles[index].name);        
+      }
+      this.banderaTabla =true;
+      //Aquí puedes manejar el archivo seleccionado, por ejemplo, enviarlo a un servidor.Implementa tu lógica de subida, por ejemplo con un servicio HTTP
+    } else {
+      console.log('No se ha seleccionado ningún archivo');
+    }
   }
 
 }
--- src/app/shared/sidebar/sidebar-routes.config.ts
+++ src/app/shared/sidebar/sidebar-routes.config.ts
@@ -3,41 +3,29 @@ import { RouteInfo } from './sidebar.metadata';
 //Sidebar menu Routes and data
 export const ROUTES: RouteInfo[] = [
 
-      
     {
         path: '/rfq', title: 'Nueva RFQ', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-    },
-    {
+    }, {
         path: '/panel', title: 'Panel de control', icon: 'bi bi-house-door', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-    },
-    {
+    }, {
         path: '/rfqs', title: 'RFQs', icon: 'lni lni-money-location', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-    },
-    {
+    }, {
         path: '/buys', title: 'Tus Compras', icon: 'lni lni-cart', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-    },
-    {
+    }, {
         path: '/projectsz', title: 'Tus Proyectos', icon: 'lni lni-grid-alt', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-    },
-    {
+    }, {
         path: '/facturas', title: 'Facturas', icon: 'lni lni-grid-alt', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-    },
-    {
+    }, {
         path: '/term', title: 'Term. & Condiciones', icon: 'lni lni-highlight-alt', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-    },
-    {
+    }, {
         path: '', title: 'Preguntas frecuentes', icon: 'lni lni-question-circle', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-    },
-    {
+    }, {
         path: '', title: 'Contactanos', icon: 'bi bi-headset', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-    },
-    {
+    }, {
         path: 'dashboard/eCommerce', title: 'Registro Cliente', icon: 'bi bi-headset', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-    },
-    {
+    }, {
         path: 'dashboard/eCommerce', title: 'Registro Proveedor', icon: 'bi bi-headset', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-    },
-    {
+    }, {
         path: 'dashboard/eCommerce', title: 'v.20924', icon: 'bi bi-headset', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
     },
 ];
