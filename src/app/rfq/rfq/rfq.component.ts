03680faa63123516e25db863fd2b35c7c752efa2
leordzgcia <leongarzagarza@hotmail.com>
2024-09-08 02:47:50 -0600

    Tabla piezas normales y espejo

--- src/app/rfq/rfq/rfq.component.ts
+++ src/app/rfq/rfq/rfq.component.ts
@@ -8,11 +8,44 @@ import { Component, OnInit } from '@angular/core';
 export class RfqComponent implements OnInit {
 
   selectedFiles: File[] = [];
-  banderaTabla = false;
-  
+  banderaTablaP = false;
+  banderaTablaPEspejo = true;
+  listaPiezasOriginales = [
+    { item: '',
+      nombre: '',
+      dibujo2d: '',
+      dibujo3d: '',
+      cantidad: null }
+  ];
 
+  listaPiezasEspejo = [
+    { item: '',
+      nombre: '',
+      dibujo2d: '',
+      dibujo3d: '',
+      cantidad: null }
+  ];
 
-  constructor() { }
+  nuevoElemento = { 
+    item: '',
+    nombre: '',
+    dibujo2d: '',
+    dibujo3d: '',
+    cantidad: null 
+  };
+
+  editing: boolean[] = [];
+  users = [
+    { name: 'Juan Pérez', email: 'juan@example.com' },
+    { name: 'Ana García', email: 'ana@example.com' },
+    { name: 'Luis Rodríguez', email: 'luis@example.com' }
+  ];
+  totalPiezas: number = 0;
+  totalPiezasEspejo: number = 0;
+
+  constructor() {
+    this.editing = this.users.map(() => false);
+  }
 
   ngOnInit(): void {
     console.log(' -- ngOnInit  RfqComponent ');
@@ -24,22 +57,53 @@ export class RfqComponent implements OnInit {
     $.getScript('./assets/js/custom-file-upload.js');
   }
 
+  // Cambia el estado de la fila a modo de edición
+  editRow(index: number): void {
+    this.editing[index] = true;
+  }
+  // Guarda los cambios y desactiva el modo de edición
+  saveRow(index: number): void {
+    this.editing[index] = false;
+    console.log('Datos guardados:', this.users[index]);
+  }
+  // Cancela la edición y restaura los valores originales
+  cancelEdit(index: number): void {
+    this.editing[index] = false;
+    console.log('Edición cancelada');
+    // Si necesitas restaurar el estado original de los datos, puedes hacerlo aquí
+  }
   onFilesSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       this.selectedFiles = Array.from(input.files);
       console.log('Archivos seleccionados:', this.selectedFiles);
+      this.onUpload();
     }
   }
-
-
-  onUpload(): void {
-    if (this.selectedFiles.length > 0 ) {
+  onUpload(): void {    
+    console.log(this.listaPiezasOriginales);
+    if (this.selectedFiles.length > 0) {
+      this.listaPiezasOriginales.pop();
+      console.log(this.listaPiezasOriginales);
       console.log('Subiendo archivo:', this.selectedFiles);
       for (let index = 0; index < this.selectedFiles.length; index++) {
-        console.log('Subiendo archivo:', this.selectedFiles[index].name);        
+        console.log('contador ' + (index + 1));
+        //console.log('Subiendo archivo:', this.selectedFiles[index].name);
+        this.nuevoElemento.item = (index + 1).toString();
+        this.nuevoElemento.nombre = this.selectedFiles[index].name;
+        this.nuevoElemento.dibujo2d = 'Disponible';
+        this.nuevoElemento.dibujo3d = 'Disponibles';
+        this.nuevoElemento.cantidad = 1;
+
+        this.listaPiezasOriginales.push(this.nuevoElemento);
+        this.totalPiezas++;
+        console.log(index);
+        console.log(this.nuevoElemento);
+        this.nuevoElemento = { item: '', nombre: '', dibujo2d: '', dibujo3d: '', cantidad: null }; // Limpiar formulario
+        console.log(this.nuevoElemento);
+
+        this.banderaTablaP = true;
       }
-      this.banderaTabla =true;
       //Aquí puedes manejar el archivo seleccionado, por ejemplo, enviarlo a un servidor.Implementa tu lógica de subida, por ejemplo con un servicio HTTP
     } else {
       console.log('No se ha seleccionado ningún archivo');
