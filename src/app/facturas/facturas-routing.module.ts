9b5fc2e9a02876252d18f483f60ba306da7c8876
leordzgcia <leongarzagarza@hotmail.com>
2024-09-14 06:42:28 -0600

    Componente facturas

---
+++ src/app/facturas/facturas-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FacturasComponent } from './facturas/facturas.component';
+
+const routes: Routes = [
+  { path : '', component : FacturasComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class FacturasRoutingModule { }
