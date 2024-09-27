91a5ec0f0e7fcf1ca0ec621cececda6441d9fd36
leordzgcia <leongarzagarza@hotmail.com>
2024-09-27 02:39:34 -0600

    Componente ambientes

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  
+  //serverApiUrl: 'http:tek10-api-lr-production.up.railway.app'
+  serverApiUrl: 'http://localhost:8080'
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  
+  serverApiUrl: 'http:tek10-api-lr-production.up.railway.app'
+  //serverApiUrl: 'http://localhost:8080'
 };
