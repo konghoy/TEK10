5bc89151b61b7436776fb39ff6c269bb9cfd90d6
leordzgcia <leongarzagarza@hotmail.com>
2024-09-27 02:38:53 -0600

    Componente Panel

--- src/app/panel/panel/panel.component.ts
+++ src/app/panel/panel/panel.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import * as chartsData from '../../shared/data/default-dashboard';
-
-import ApexCharts from 'apexcharts/dist/apexcharts.common.js';
 import { FormBuilder } from '@angular/forms';
 import { CotizacionService } from 'src/app/services/cotizacion.service';
 import { DataService } from 'src/app/services/data.service';
 import { Cotizacion } from 'src/app/interfaces/Cotizacion';
+import { ProjectoService } from 'src/app/services/projecto.service';
+import { Proyecto } from 'src/app/interfaces/Proyecto';
 
 
 @Component({
@@ -17,29 +17,31 @@ export class PanelComponent implements OnInit {
 
   //listaCotizaciones: Cotizacion ;
   listaCotizaciones: Cotizacion [] = [];
+  listaProyectos: Proyecto [] = [];
 
   constructor(
-    private api: CotizacionService,
+    private apiCotizacion: CotizacionService,
+    private apiProjecto: ProjectoService,
     private dataService: DataService
   ) { 
-   
   }
 
   ngOnInit(): void {
     console.log(' -- ngOnInit  PanelComponent ');
     //$.getScript("./assets/js/digital-marketing.js");
     //$.getScript("./assets/js/alternate-dashboard.js");
-    this.obtenerCotizacione();
+    this.obtenerCotizaciones();
+    this.obtenerProyectos();
     //LDRG
     
   }
 
 
-  obtenerCotizacione(){
-    this.api.obtenerCotizacions().subscribe(
+  obtenerCotizaciones(){
+    this.apiCotizacion.obtenerCotizaciones().subscribe(
       (data) => {
         //this.arrayExamenes = data;
-        
+  
         this.listaCotizaciones = data;
         console.log(data.length);
         console.log(this.listaCotizaciones);
@@ -54,6 +56,25 @@ export class PanelComponent implements OnInit {
       }
     );
 
+  }
+
+  obtenerProyectos(){
+    this.apiProjecto.obtenerProyectos().subscribe(
+      (data) => {
+        //this.arrayExamenes = data;
+        this.listaProyectos = data;
+        console.log(data);
+        console.log(this.listaProyectos);
+        /*for (let index = 0; index < data.length; index++) {
+          console.log(data[index].idMateria);
+          this.arrayExamenes.push(data[index].idExamen + ' - ' + data[index].idMateria);
+          this.arrayExamenesInt.push({ idExamen: data[index].idExamen, nombreExamen: data[index].nombreExamen });
+        }*/
+      },
+      (error) => {
+        console.error('Error fetching data list:', error);
+      }
+    );
   }
 
 }
